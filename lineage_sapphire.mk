e2f830b009c3c53052b03aa8b7daeedb6ed5a2ba
superuseryu <superuseryu@proton.me>
2026-09-06 02:00:05 +0000

    product: inherit MindTheGapps arm64-vendor.mk
    GApps was never wired into the build - WITH_GMS=true in
    lineage_sapphire.mk targets Google's Partner GMS mechanism
    (partner_gms.mk), which is unrelated to the synced vendor/gapps
    (MindTheGapps) tree. No inherit-product line existed anywhere
    pulling from vendor/gapps. Added direct inherit of arm64-vendor.mk.

--- lineage_sapphire.mk
+++ lineage_sapphire.mk
@@ -46,3 +46,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sapphire_global-user 15 AQ3A.240829.003 OS2.0.211.0.VNGMIXM release-keys" \
     BuildFingerprint=Redmi/sapphire_global/sapphire:15/AQ3A.240829.003/OS2.0.211.0.VNGMIXM:user/release-keys
+# GApps (MindTheGapps)
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
