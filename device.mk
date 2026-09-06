5338f666fd638f5fc828cebdc8cfa0d72fd40b05
superuseryu <superuseryu@proton.me>
2026-09-06 06:47:10 +0000

    device: enable fast charge toggle via restrict_chg node

--- device.mk
+++ device.mk
@@ -378,6 +378,11 @@ $(call soong_config_set, lineage_health, charging_control_charging_enabled, 0)
 $(call soong_config_set, lineage_health, charging_control_charging_disabled, 1)
 $(call soong_config_set, lineage_health, charging_control_supports_bypass, false)
 
+# Fast Charge
+$(call soong_config_set, lineage_health, fast_charge_node, /sys/class/qcom-battery/restrict_chg)
+$(call soong_config_set, lineage_health, fast_charge_value_none, 1)
+$(call soong_config_set, lineage_health, fast_charge_value_fast_charge, 0)
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm
